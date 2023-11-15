69194798afc7eeca1812cd7094e0a04afda6bba4
678287 <142848880+678287@users.noreply.github.com>
2023-11-15 16:16:20 +0100

    Update Tekst.java

--- src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
@@ -4,40 +4,26 @@ import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Tekst extends Innlegg {
+	private String tekst;
 
-	// TODO: objektvariable 
-	
-	public Tekst () {
-		
-	}
-	
-	public Tekst(int id, String bruker, String dato, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
-	}
-	
-	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
-	}
-	
-	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
+    public Tekst(int id, String bruker, String dato, int likes, String tekst) {
+        super(id, bruker, dato, likes);
+        this.tekst = tekst;
+    }
+    
+//public get/set metoder for tekst klassen
+    
+    public String getTekst() {
+        return tekst;
+    }
+    
 
-	}
-
-	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
-
-	@Override
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-
-	}
-	
-	// Metoden nedenfor er kun for valgfri oppgave 6
-	public String toHTML() {
-			
-		throw new UnsupportedOperationException(TODO.method());
-				
-	}
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
+    
+        
+        public String toString() {
+            return "TEKST" + "\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "nen " + getTekst() + "\n" ;
 }
+    }
