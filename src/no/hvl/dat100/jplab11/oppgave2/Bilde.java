e35c8a90ff84a5da8e656d04f4d116260e6b2135
678291 <678291@stud.hvl.no>
2023-11-20 14:30:42 +0100

    fix

--- src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
@@ -3,26 +3,43 @@ package no.hvl.dat100.jplab11.oppgave2;
 import no.hvl.dat100.jplab11.common.TODO;
 
 public class Bilde extends Tekst {
-        private String url;
+	
+	// TODO - objekt variable
+	
+		private String url; 
+		
+		public Bilde(int id, String bruker, String dato, String tekst, String url) {
+			super(id, bruker,dato, tekst);
+			this.url = url;
+		}
 
-        public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-            super(id, bruker, dato, likes, tekst);
-            this.url = url;
-        }
-        
-//public get/set metoder for bilde klassen
-        
-        public String getUrl() {
-            return url;
-        }
+		public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
+			super(id, bruker, dato, likes, tekst);
+			this.url = url;
+		}
+		
+		public String getUrl() {
+			return this.url;
+		}
 
-        public void setUrl(String url) {
-            this.url = url;
-        }
+		public void setUrl(String url) {
+			this.url = url;
+		}
 
-        public String toString() {
-             return "BILDE" + "\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "net Bildet\n" + getUrl() + "\n";
-        }
-}
-        
+		@Override
+		public String toString() {
+			
+			return "BILDE\n" + getId() + "\n" + getBruker() + "\n" +
+					getDato() + "\n" + getLikes() + "\n" + getTekst() + "\n" + url + "\n";
+
+
+		}
+
+		// Metoden nedenfor er kun for valgfri oppgave 6
+		public String toHTML() {
+			
+			throw new UnsupportedOperationException(TODO.method());
+					
+		}
+	}
 
--- src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Tekst.java
@@ -4,26 +4,42 @@ import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Tekst extends Innlegg {
-	private String tekst;
+	
+	// TODO: objektvariable 
+		private String tekst;
 
-    public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-        super(id, bruker, dato, likes);
-        this.tekst = tekst;
-    }
-    
-//public get/set metoder for tekst klassen
-    
-    public String getTekst() {
-        return tekst;
-    }
-    
+		public Tekst () {
+			
+		}
+		
+		public Tekst(int id, String bruker, String dato, String tekst) {
+			super(id, bruker, dato);
+			this.tekst = tekst;
+		}
+		
+		public Tekst(int id, String bruker, String dato, int likes, String tekst) {
+			super(id, bruker, dato, likes);
+			this.tekst = tekst;
+		}
+		
+		public String getTekst() {
+			return this.tekst; 
+		}
 
-    public void setTekst(String tekst) {
-        this.tekst = tekst;
-    }
-    
-        
-        public String toString() {
-            return "TEKST" + "\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "nen " + getTekst() + "\n" ;
-}
-    }
+		public void setTekst(String tekst) {
+			this.tekst = tekst;
+		}
+
+		@Override
+		public String toString() {
+			return "TEKST\n" + getId() + "\n" + getBruker() + "\n" +
+					getDato() + "\n" + getLikes() + "\n" + tekst + "\n";
+		}
+		
+		// Metoden nedenfor er kun for valgfri oppgave 6
+		public String toHTML() {
+				
+			throw new UnsupportedOperationException(TODO.method());
+					
+		}
+	}
