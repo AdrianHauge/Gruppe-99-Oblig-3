f504b58dc0c4e3e4a4fd78a1187b757dc73878e5
678281 <142600098+678281@users.noreply.github.com>
2023-11-20 17:02:57 +0100

    oppdatert

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -6,51 +6,53 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 public class Blogg {
 
 	private Innlegg[] innleggtabell;
-    private int nesteledig; 
+	private int nesteledig;
 
 	public Blogg() {
 		this.innleggtabell = new Innlegg[20];
-        this.nesteledig = 0;
+		this.nesteledig = 0;
 	}
 
 	public Blogg(int lengde) {
 		this.innleggtabell = new Innlegg[lengde];
-        this.nesteledig = 0;
+		this.nesteledig = 0;
 	}
 
 	public int getAntall() {
-		return nesteledig;	}
-	
+		return nesteledig;
+	}
+
 	public Innlegg[] getSamling() {
 		return innleggtabell;
 	}
-	
+
 	public int finnInnlegg(Innlegg innlegg) {
 
 		for (int i = 0; i < nesteledig; i++) {
-            if (innleggtabell[i].erLik(innlegg)) {
-                return i;	
-                }
+			if (innleggtabell[i].erLik(innlegg)) {
+				return i;
+			}
 		}
 		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		return finnInnlegg(innlegg) != -1;	}
+		return finnInnlegg(innlegg) != -1;
+	}
 
 	public boolean ledigPlass() {
 		return nesteledig < innleggtabell.length;
 	}
-	
+
 	public boolean leggTil(Innlegg innlegg) {
 
 		if (ledigPlass() && !finnes(innlegg)) {
-            innleggtabell[nesteledig] = innlegg;
-            nesteledig++;
-            return true;
-        } else {
-            return false;
-        }
+			innleggtabell[nesteledig] = innlegg;
+			nesteledig++;
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public String toString() {
