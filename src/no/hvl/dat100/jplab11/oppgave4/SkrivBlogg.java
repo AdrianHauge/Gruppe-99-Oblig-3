86d54b56d911fbdaacb66bef5d2dade153b951cf
678290 <Shahoz2003@gmail.com>
2023-11-20 15:28:40 +0100

    oppg 4

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -4,12 +4,14 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import javax.swing.JOptionPane;
 import java.io.File;
+
+import no.hvl.dat100.jplab11.oppgave1.Innlegg;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
 	private static String mappe = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
-	
+
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
 		String fil = filnavn;
@@ -21,6 +23,14 @@ public class SkrivBlogg {
 		skriver.println(samling.toString());
 		skriver.close();
 		
+		Innlegg[] innleggArray = samling.getSamling();
+		
+		skriver.println(samling.getAntall());
+		
+		for (int i = 0; i < samling.getAntall(); i++) {
+			skriver.println (innleggArray[i].toString());
+		}
+		
 		return true;
 		
 		} catch (FileNotFoundException e) {
@@ -28,5 +38,11 @@ public class SkrivBlogg {
 			return false;
 		}
 		
+	}
+
+	public static void main(String[] args) {
+		Blogg blogg = new Blogg();
+
+		skriv(blogg, "Bloggen", "blogg.dat");
 	}
 }
